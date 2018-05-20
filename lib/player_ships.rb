47ec681423efc27b1e1fa00c5177c62fd0afef71
Angela Duran <duranangela19@gmail.com>
2018-05-19 19:57:40 -0600

    Got player input small ship to work with validate ships

--- lib/player_ships.rb
+++ lib/player_ships.rb
@@ -4,31 +4,32 @@ require 'pry'
 
 class PlayerShips < Board
   include ValidateShips
+  attr_reader :smship, :bgship
 
   def initialize
     super
+    @smship = []
+    @bgship = []
   end
 
-  def get_2ship
+  def get_small_ship
     puts message1
-    spot1 = gets.chomp.chars
-    validate_exists(spot1)
-    @layout[spot1] = 'S '
-    puts message2
-    spot2 = gets.chomp.chars
-    validate_exists(spot2)
-    # validate_place(spot1, spot2)
-    @layout[spot2] = 'S '
-
+    @smship = gets.chomp.split(' ')
+    until validate_small(@smship)
+      puts "Invalid choice, please try again: "
+      @smship = gets.chomp.split(' ')
+    end
+    @layout[@smship[0]] = 'S '
+    @layout[@smship[1]] = 'S '
     puts board
   end
 
   def message1
-    "I have laid out my ships on the grid.\nYou now need to layout your two ships.\nThe first is two units long and the second is three units long.\nThe grid has A1 at the top left and D4 at the bottom right.\n\nEnter the first square for the two_unit ship: "
+    "I have laid out my ships on the grid.\nYou now need to layout your two ships.\nThe first is two units long and the second is three units long.\nThe grid has A1 at the top left and D4 at the bottom right.\n\nEnter the squares for the two_unit ship, for example 'A1 A2': "
   end
 
   def message2
-    "Enter the second square.\nIt must be adjacent to the first square: "
+
   end
 
 end
--- lib/validate_ships.rb
+++ lib/validate_ships.rb
@@ -1,6 +1,45 @@
 
 module ValidateShips
 
+  def validate_small(smship)
+    if smship[0] == 'A1'
+      ['A2', 'B1'].include? smship[1]
+    elsif smship[0] == 'A2'
+      ['A1', 'B2', 'A2'].include? smship[1]
+    elsif smship[0] == 'A3'
+      ['A2', 'B3', 'A4'].include? smship[1]
+    elsif smship[0] == 'A4'
+      ['A3', 'B4'].include? smship[1]
+    elsif smship[0] == 'B1'
+      ['A1', 'B2', 'C1'].include? smship[1]
+    elsif smship[0] == 'B2'
+      ['A2', 'B1', 'C2', 'B3'].include? smship[1]
+    elsif smship[0] == 'B3'
+      ['A3', 'B2', 'C3', 'B4'].include? smship[1]
+    elsif smship[0] == 'B4'
+      ['A4', 'B3', 'C4'].include? smship[1]
+    elsif smship[0] == 'C1'
+      ['B1', 'C2', 'D1'].include? smship[1]
+    elsif smship[0] == 'C2'
+      ['B2', 'C1', 'D2', 'C3'].include? smship[1]
+    elsif smship[0] == 'C3'
+      ['B3', 'C2', 'D3', 'C4'].include? smship[1]
+    elsif smship[0] == 'C4'
+      ['B4', 'C3', 'D4'].include? smship[1]
+    elsif smship[0] == 'D1'
+      ['C1', 'D2'].include? smship[1]
+    elsif smship[0] == 'D2'
+      ['C2', 'D1', 'D3'].include? smship[1]
+    elsif smship[0] == 'D3'
+      ['C3', 'D2', 'D4'].include? smship[1]
+    elsif smship[0] == 'D4'
+      ['C4', 'D3'].include? smship[1]
+    else
+      false
+    end
+  end
+
+
   def validate_exists(spot)
     until @layout.include?(spot)
       puts "Invalid choice, try again"
