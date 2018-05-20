b56d828e5e86241b12c2bc641ae857e59bac9b71
Angela Duran <duranangela19@gmail.com>
2018-05-19 19:03:50 -0600

    Fixed return issues in compships file

--- lib/comp_ships.rb
+++ lib/comp_ships.rb
@@ -12,36 +12,21 @@ attr_reader :smship, :bgship
   def small_ship
     first = (0..14).to_a.sample
     case first
-    when 0
-      second = [1, 4].sample
-    when 1
-      second = [2, 5].sample
-    when 2
-      second = [3, 6].sample
-    when 3
-      second = 7
-    when 4
-      second = [5, 8].sample
-    when 5
-      second = [6, 9].sample
-    when 6
-      second = [7, 10].sample
-    when 7
-      second = 11
-    when 8
-      second = [9, 12].sample
-    when 9
-      second = [10, 13].sample
-    when 10
-      second = [11, 14].sample
-    when 11
-      second = 15
-    when 12
-      second = 13
-    when 13
-      second = 14
-    when 14
-      second = 15
+    when 0 then second = [1, 4].sample
+    when 1 then second = [2, 5].sample
+    when 2 then second = [3, 6].sample
+    when 3 then second = 7
+    when 4 then second = [5, 8].sample
+    when 5 then second = [6, 9].sample
+    when 6 then second = [7, 10].sample
+    when 7 then second = 11
+    when 8 then second = [9, 12].sample
+    when 9 then second = [10, 13].sample
+    when 10 then second = [11, 14].sample
+    when 11 then second = 15
+    when 12 then second = 13
+    when 13 then second = 14
+    when 14 then second = 15
     end
     @smship << @layout.keys[first]
     @smship << @layout.keys[second]
@@ -49,10 +34,8 @@ attr_reader :smship, :bgship
   end
 
   def big_ship
-    array = []
-    ship = []
-    until array.length == 3 && (array.none? {|x| @smship.include?(x)})
-      array = []
+    until @bgship.length == 3 && (@bgship.none? {|x| @smship.include?(x)})
+      @bgship = []
       index1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13].sample
       case index1
       when 0
@@ -96,13 +79,10 @@ attr_reader :smship, :bgship
         index2 = 14
         index3 = 15
       end
-      array << @layout[@layout.keys[index1]]
-      array << @layout[@layout.keys[index2]]
-      array << @layout[@layout.keys[index3]]
+      @bgship << @layout.keys[index1]
+      @bgship << @layout.keys[index2]
+      @bgship << @layout.keys[index3]
     end
-    @bgship << @layout.keys[index1]
-    @bgship << @layout.keys[index2]
-    @bgship << @layout.keys[index3]
     @bgship
   end
 
