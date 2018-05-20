8ef1dbe23058ba8d94c87bee23cee1662b6fe378
Angela Duran <duranangela19@gmail.com>
2018-05-20 10:41:12 -0600

    Finished play file, added game over file

---
+++ lib/game_over.rb
@@ -0,0 +1,11 @@
+module GameOver
+
+  def over_winner
+    "You've sunk all of my ships, congratulations!"
+  end
+
+  def over_loser
+    "Sorry, but I've sunk all of your ships. Better luck next time!"
+  end
+  
+end
--- lib/play.rb
+++ lib/play.rb
@@ -1,7 +1,10 @@
 require './lib/comp_shots.rb'
 require './lib/player_shots.rb'
+require './lib/game_over'
+require 'pry'
 
 class Play
+  include GameOver
 
   def initialize
     @compshot = CompShots.new
@@ -11,9 +14,14 @@ class Play
   def play_game
     @playershot.ships
     @compshot.ship_board
-
-    @playershot.take_shot
-    @compshot.comp_shot
+    loop do
+      @playershot.take_shot
+      break if @playershot.hitboard.layout.values.count('H ') == 5
+      @compshot.comp_shot
+      break if @compshot.player_ships.layout.values.count('H ') == 5
+    end
+    puts over_winner if @playershot.hitboard.layout.values.count('H ') == 5
+    puts over_loser if @compshot.player_ships.layout.values.count('H ') == 5
   end
-  
+
 end
