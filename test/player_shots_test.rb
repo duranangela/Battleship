4b76225c37437f7340903512f4cb8d0354824003
Angela Duran <duranangela19@gmail.com>
2018-05-22 17:14:38 -0600

    Updated files to reflect new board layout

--- test/player_shots_test.rb
+++ test/player_shots_test.rb
@@ -7,5 +7,12 @@ class PlayerShotsTest < Minitest::Test
     shots = PlayerShots.new
     assert_instance_of PlayerShots, shots
   end
-  
+
+  def test_it_can_get_comp_ships
+    shots = PlayerShots.new
+    shots.ships
+    assert_equal 2, shots.comp_ships.smship.length
+    assert_equal 3, shots.comp_ships.bgship.length
+  end
+
 end
