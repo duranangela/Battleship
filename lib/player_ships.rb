de6a77128cf465fcb4ac580d6b4c092b3143eda2
Angela Duran <duranangela19@gmail.com>
2018-05-22 20:09:07 -0600

    Refactored player ships and validate ships

--- lib/player_ships.rb
+++ lib/player_ships.rb
@@ -23,32 +23,57 @@ class PlayerShips < Board
   end
 
   def small_ship
-    @smship = gets.chomp.split(' ')
+    @smship = gets.chomp.upcase.split(' ')
     until validate_small(@smship)
       puts "Invalid choice, please try again: "
-      @smship = gets.chomp.split(' ')
+      @smship = gets.chomp.upcase.split(' ')
     end
-    @layout[@smship[0]] = 'S '
-    @layout[@smship[1]] = 'S '
+    @smship[0] = @layout.find { |spot| spot[1] == @smship[0]}
+    @smship[1] = @layout.find { |spot| spot[1] == @smship[1]}
+    @smship[0][2] = 'S '
+    @smship[1][2] = 'S '
   end
 
   def big_ship
-    loop do
-      @bgship = []
-      @bgship = gets.chomp.split(' ')
-      until validate_big(@bgship)
-        puts "Invalid choice, please try again: "
-        @bgship = gets.chomp.split(' ')
+    until (@smship + @bgship).uniq.length == 5
+      @bgship = gets.chomp.upcase.split(' ')
+      loop do
+        if validate_big_spots(@bgship)
+          @bgship << get_second(@bgship)
+          break
+        else
+          puts "Invalid choice, please try again: "
+          @bgship = gets.chomp.upcase.split(' ')
+        end
       end
-      spot = get_middle_spot(@bgship)
-      @bgship << spot
-      break if (@smship + @bgship).uniq.length == 5
-      puts "Ships can't overlap, please try again: "
+      @bgship[0] = layout.find { |spot| spot[1] == @bgship[0]}
+      @bgship[1] = layout.find { |spot| spot[1] == @bgship[1]}
     end
-    @layout[@bgship[0]] = 'S '
-    @layout[@bgship[1]] = 'S '
-    @layout[@bgship[2]] = 'S '
+    @bgship[0][2] = 'S '
+    @bgship[1][2] = 'S '
+    @bgship[2][2] = 'S '
   end
+
+  #   loop do
+  #     @bgship = []
+  #     @bgship = gets.chomp.split(' ')
+  #     until validate_big_spots(@bgship)
+  #       puts "Choices must be on the board, please try again: "
+  #       @bgship = gets.chomp.split(' ')
+  #     end
+  #     until validate_third(@bgship)
+  #       puts ""
+  #
+  #
+  #     spot = get_middle_spot(@bgship)
+  #     @bgship << spot
+  #     break if (@smship + @bgship).uniq.length == 5
+  #     puts "Ships can't overlap, please try again: "
+  #   end
+  #   @layout[@bgship[0]] = 'S '
+  #   @layout[@bgship[1]] = 'S '
+  #   @layout[@bgship[2]] = 'S '
+  # end
 
   def message1
     "\n\nI have laid out my ships on the grid.\nYou now need to layout your two ships.\nThe first is two units long and the second is three units long.\nThe grid has A1 at the top left and D4 at the bottom right.\n\nEnter the two squares for the two-unit ship, for example 'A1 A2': "
